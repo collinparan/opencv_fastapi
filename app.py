6097e1d8763eca39729841c7cccaf4d58c9232e4
Collin <39640531+statsRcool@users.noreply.github.com>
2022-08-15 23:56:17 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+
+import cv2
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import StreamingResponse
+
+app = FastAPI()
+camera = cv2.VideoCapture(0)
+templates = Jinja2Templates(directory="templates")
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.get('/')
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get('/video_feed')
+def video_feed():
+    return StreamingResponse(gen_frames(), media_type='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=9000, debug=True)
